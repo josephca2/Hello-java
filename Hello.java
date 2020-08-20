ae2321cbb7e710e58893e1dd6cb653ac9469f4ba
josephca2 <69979449+josephca2@users.noreply.github.com>
2020-08-20 11:53:08 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,18 @@
+**************************************************************
+* Hello Java
+**************************************************************
+* Christina Joseph
+* August 20 2020
+* CMSC255 905
+**************************************************************
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("<Your Name>");
+		System.out.println("<Java Project Name>");
+		System.out.println("<Project Description>");
+		System.out.println("<Version Date>");
+		System.out.println("<Course Number and Section>");
+		System.out.println();
+	}
+
+}
