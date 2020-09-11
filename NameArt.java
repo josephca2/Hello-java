303777a0c7e530e22a4e393aff1d5a4784e58540
josephca2 <69979449+josephca2@users.noreply.github.com>
2020-09-10 20:29:22 -0400

    Add files via upload

---
+++ NameArt.java
@@ -0,0 +1,20 @@
+/**********************************
+* Project 1 - Name in Code
+*
+* This project outputs my nickname (Christy) in a unique way on the display screen.
+*
+* Christina Joseph
+* August 25, 2020
+* CMSC255 001
+***********************************/
+public class NameArt {
+	public static void main(String[] arg) {
+
+		System.out.println("   CCCCC  HH  HH  RRRRR    II   SSSS  TTTTTT  YY   YY   !!   ");
+		System.out.println("  CC      HH  HH  RR  RR   II  SSS      TT     YY YY    !!   ");
+		System.out.println(" CC       HHHHHH  RR RR    II   SSS     TT      YYY     !!   ");
+		System.out.println("  CC      HH  HH  RR  RR   II     SS    TT      YYY          ");
+		System.out.println("   CCCCC  HH  HH  RR   RR  II  SSSS     TT      YYY     ..   ");
+		
+	}
+}
